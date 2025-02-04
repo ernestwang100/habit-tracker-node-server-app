0f4651af33e40c3249a8aa82a15234ea028bbe11
Yu-En Wang <ernestwang100@gmail.com>
2025-02-04 09:56:52 -0800

    use app.use()

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import express from "express";
 import cors from "cors";
-import HabitLogRoutes from "./Habitron/habitLogs/routes.js";
-import HabitRoutes from "./Habitron/habits/routes.js";
+import habitLogRoutes from "./Habitron/habitLogs/routes.js";
+import habitRoutes from "./Habitron/habits/routes.js";
 const app = express();
 app.use(cors());
 // Add middleware to parse JSON body
 app.use(express.json());  // This is the key line that ensures `req.body` is parsed
-HabitLogRoutes(app);
-HabitRoutes(app);
+
+// Use the routes
+app.use(habitLogRoutes);
+app.use(habitRoutes);
 // Start the server
 const PORT = 4000;
 app.listen(PORT, () => {
