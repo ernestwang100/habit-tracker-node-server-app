01e24f561c30d4f266f56a5aace2a157d43cc2a5
Yu-En Wang <ernestwang100@gmail.com>
2025-02-04 11:26:21 -0800

    register routes with prefixes in App.js

--- App.js
+++ App.js
@@ -7,9 +7,10 @@ app.use(cors());
 // Add middleware to parse JSON body
 app.use(express.json());  // This is the key line that ensures `req.body` is parsed
 
-// Use the routes
-app.use(habitLogRoutes);
-app.use(habitRoutes);
+// Register routes with prefixes
+app.use("/api/habitlogs", habitLogRoutes);
+app.use("/api/habits", habitRoutes);
+
 // Start the server
 const PORT = 4000;
 app.listen(PORT, () => {
