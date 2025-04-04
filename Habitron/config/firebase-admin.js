38c01ea3769abf3b4d6d40569ec61e324cd4ab96
Yu-En Wang <ernestwang100@gmail.com>
2025-04-04 16:11:24 -0700

    Use Heroku Config Vars for Firebase key

--- Habitron/config/firebase-admin.js
+++ Habitron/config/firebase-admin.js
@@ -1,6 +1,6 @@
 import admin from 'firebase-admin';
 // import serviceAccount from './serviceAccountKey.json' assert { type: 'json' };
-const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
